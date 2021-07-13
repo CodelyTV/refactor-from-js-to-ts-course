1ac264ab0cfdda8d576c658efeabb893f90b0edb
Nuria <soriano.nuria@gmail.com>
2021-07-13 10:38:41 +0200

    lesson 7-3: vue app with typescript

---
+++ 73-vue-frontend/src/components/CourseInfo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="course-card">
+    <CourseImage :image="course.image" :title="course.title" />
+    <div class="course-card__info">
+      <h6 class="course-card__title">
+        <a :href="course.link" rel="noreferrer" target="_blank">
+          {{ course.title }}
+        </a>
+      </h6>
+      <strong>{{ course.teachers }}</strong>
+      <p>
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { Course } from "@/types";
+import CourseImage from "@/components/CourseImage.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    CourseImage,
+  },
+  props: {
+    course: {
+      type: Object as PropType<Course>,
+      required: true,
+    },
+  },
+  computed: {
+    description(): string {
+      return this.course.description.length > 70
+        ? this.course.description.substr(0, 70) + "…"
+        : this.course.description;
+    },
+  },
+});
+</script>
